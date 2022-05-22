e0fefe1644d87165fdd815336ab5f4f051a3495c
Adam Shea <avidworks@live.com>
2022-05-21 23:43:18 -0400

    fix links

--- src/App.jsx
+++ src/App.jsx
@@ -21,26 +21,28 @@ const App = () => {
 	return (
 		<div className={theme ? 'dark' : 'light'}>
 			<div className=" | min-h-screen | py-9 px-6 | bg-zircon text-kashmir dark:bg-bigstone dark:text-white">
-				<div className="flex flex-col | max-w-xl lg:max-w-[730px] | gap-4 sm:gap-6 mx-auto md:mt-36">
+				<div className="grid | max-w-xl lg:max-w-[730px] | gap-4 sm:gap-6 mx-auto md:mt-36">
 					<Header />
-					<Search handleSubmit={handleSubmit} />
-					<main className="grid lg:grid-cols-[117px_1fr] | gap-6 px-6 py-8 sm:p-12 sm:gap-8 | bg-darkwhite dark:bg-cloudburst | shadow-block dark:shadow-none | rounded-xl">
-						{error && (
-							<div className="mr-4 | text-sunset whitespace-nowrap font-bold">No results</div>
-						)}
-						{isLoading && <p>Loading user...</p>}
-						{user && (
-							<>
-								<div className="col-span-full | grid grid-cols-[min-content_1fr] items-center lg:items-start | gap-4 sm:gap-9">
-									<Avatar user={user} />
-									<UserHeader user={user} />
-								</div>
-								<div className="col-span-full lg:col-start-2 | flex flex-col | gap-6 sm:gap-9 lg:-mt-16">
-									<UserInfo user={user} />
-									<UserLinks user={user} />
-								</div>
-							</>
-						)}
+					<main className="grid | gap-4 sm:gap-6">
+						<Search handleSubmit={handleSubmit} />
+						<section className="grid lg:grid-cols-[117px_1fr] | gap-6 px-6 py-8 sm:p-12 sm:gap-8 | bg-darkwhite dark:bg-cloudburst | shadow-block dark:shadow-none | rounded-xl">
+							{error && (
+								<div className="mr-4 | text-sunset whitespace-nowrap font-bold">No results</div>
+							)}
+							{isLoading && <p>Loading user...</p>}
+							{user && (
+								<>
+									<div className="col-span-full | grid grid-cols-[min-content_1fr] items-center lg:items-start | gap-4 sm:gap-9">
+										<Avatar user={user} />
+										<UserHeader user={user} />
+									</div>
+									<div className="col-span-full lg:col-start-2 | flex flex-col | gap-6 sm:gap-9 lg:-mt-16">
+										<UserInfo user={user} />
+										<UserLinks user={user} />
+									</div>
+								</>
+							)}
+						</section>
 					</main>
 				</div>
 			</div>
