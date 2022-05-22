17c24cde68a5523514c92a147a2bded2ea09d083
Adam Shea <avidworks@live.com>
2022-05-21 23:14:25 -0400

    fix key prop error

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -19,7 +19,7 @@ const UserInfo = ({ user }) => {
 			<p className="leading-6">{user.bio}</p>
 			<div className="flex items-center justify-between | py-6 px-8 | bg-zircon dark:bg-bigstone | rounded-xl ">
 				{stats.map((stat) => (
-					<div className="flex flex-col | text-center sm:text-left">
+					<div className="flex flex-col | text-center sm:text-left" key={stat.title}>
 						<h3 className="text-[11px] sm:text-[13px] leading-tight">{stat.title}</h3>
 						<p className="text-base sm:text-[22px] font-bold text-ebony dark:text-white leading-tight mt-2">
 							{stat.value}
