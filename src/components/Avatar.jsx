4e12d8e7a2ec2c63b3cc4b0d4cbe372e32eb9510
Adam Shea <avidworks@live.com>
2022-05-22 00:07:58 -0400

    deploy

--- src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-const Avatar = ({ user }) => {
-	return (
-		<div className="w-[70px] h-[70px] sm:w-[117px] sm:h-[117px] bg-lynch |  rounded-full | overflow-hidden">
-			<img src={user.avatar_url} alt={`Picture of ${user.name}`} />
-		</div>
-	);
-};
-
-export default Avatar;
